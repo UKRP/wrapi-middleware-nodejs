166c825c14bace99e1c228009916f750ca3fff90
Quentin WENZINGER <skarnwortex@hotmail.com>
2023-05-16 13:36:15 +0200

    add log example in enum

--- enum.js
+++ enum.js
@@ -2,7 +2,12 @@
  * Add in Authorization login allowed to query data
  * example : "id:password"
  */
-const Authorization = ["stellantis:re4127mpo46ds"];
+const Authorization = [
+	"EngineeringUser1:TestPassword1",
+	"EngineeringUser2:TestPassword2",
+	"ProductionUser1:ProdPassword1",
+	"ProductionUser2:ProdPassword2",
+	];
 
 module.exports = {
 	Authorization,
